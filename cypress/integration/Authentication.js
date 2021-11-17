5ac2ed27af44f21dfc62ec8e8e97c01ad39967c9
Eddie Saunders <saunders.eddie@outlook.com>
2021-11-16 20:17:44 -0500

    Auth code update

--- cypress/integration/Authentication.js
+++ cypress/integration/Authentication.js
@@ -5,13 +5,5 @@
 describe('The Internet Authentication Tests', () => {
     it('Basic Auth', () => {
         cy.visit('https://admin:admin@the-internet.herokuapp.com/basic_auth')
-
-        // , {
-        // auth: {
-        // username: admin,
-        // password: admin
-        // }
-        // })
-
-    })
+-    })
 })
